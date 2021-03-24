879e055334bd9acef176e79ec126549f45685eec
Bailey Roach <Anequit@gmail.com>
2021-03-24 19:22:53 -0400

    Unused dependencies

--- MediaControllerLibrary/FileHandler.cs
+++ MediaControllerLibrary/FileHandler.cs
@@ -3,9 +3,6 @@ using MediaControllerLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MediaControllerLibrary
 {
