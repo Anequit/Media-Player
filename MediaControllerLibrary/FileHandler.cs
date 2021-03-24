f1e1f405a0badb9f88a5c47ab3dbb05129d1c65e
Bailey Roach <Anequit@gmail.com>
2021-03-24 08:08:44 -0400

    Update FileHandler.cs

--- MediaControllerLibrary/FileHandler.cs
+++ MediaControllerLibrary/FileHandler.cs
@@ -2,6 +2,7 @@
 using MediaControllerLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,55 @@ namespace MediaControllerLibrary
     {
         private readonly FileType fileType;
         private readonly string folderPath;
-
-        List<FileModel> FileList = new List<FileModel>();
+        readonly List<FileModel> FileList = new();
 
         public FileHandler(string folderPath, FileType fileType)
         {
             this.fileType = fileType;
             this.folderPath = folderPath;
-
-            GetFiles(folderPath);
         }
 
-        private void GetFiles(string folderPath)
+        /// <summary>
+        /// Returns a built FileList with FileModels
+        /// </summary>
+        /// <returns></returns>
+        public List<FileModel> GetFileList()
+        {
+            if(FileList == null)
+                throw new Exception("FileList is null");
+
+            return FileList;
+        }
+
+        /// <summary>
+        /// Adds files that match the extension provided at FileHandler creation.
+        /// </summary>
+        public void BuildFileList()
         {
             // Fetch files with the chosen filetype
             // map each file to a FileModel then
             // Add each supported file to the file list
+            ClearFileList();
+
+            DirectoryInfo directoryInfo = new(folderPath);
+
+            foreach(var file in directoryInfo.GetFiles())
+            {
+                if(file.Extension != fileType.ToString()) {
+                    return;
+                }
+
+                FileModel fileListItem = new()
+                {
+                    Name = file.Name,
+                    Path = file.FullName
+                };
+
+                FileList.Add(fileListItem);
+            }
         }
+
+        private void ClearFileList()
+            => FileList.Clear();
     }
 }
