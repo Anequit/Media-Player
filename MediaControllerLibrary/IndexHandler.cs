a63a1fb9f97665a618b3fa5df30e6c420d569073
Bailey Roach <Anequit@gmail.com>
2021-03-24 11:46:14 -0400

    Update IndexHandler.cs

--- MediaControllerLibrary/IndexHandler.cs
+++ MediaControllerLibrary/IndexHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,45 +9,71 @@ namespace MediaControllerLibrary
 {
     public class IndexHandler
     {
-        private int currentIndex;
+        private int currentIndex = 0;
         private int previousIndex;
 
-        private int indexMax;
-        private int indexMin;
+        private int indexMax = 0;
+        private readonly int indexMin = 0;
 
-        public IndexHandler(int max, int min)
+        public IndexHandler(List<IEnumerable> enumerables)
         {
-            this.indexMax = max;
-            this.indexMin = min;
+            UpdateIndex(enumerables);
         }
 
-        public int GetCurrentIndex()
-        {
-            // Simply return current index.
-            return currentIndex;
-        }
+        /// <summary>
+        /// Get the current index
+        /// </summary>
+        /// <returns>
+        /// Current index
+        /// </returns>
+        public int GetCurrentIndex() 
+            => currentIndex;
+        
+        /// <summary>
+        /// Updates the index max
+        /// </summary>
+        /// <param name="enumerables"></param>
+        public void UpdateIndex(List<IEnumerable> enumerables)
+            => indexMax = enumerables.Count - 1;
 
-        public int IndexBack(int currentIndex, string path, int mp3Count)
+        /// <summary>
+        /// Moves index back by 1
+        /// </summary>
+        public void IndexBack()
         {
             // Decrement currentIndex and check if it's greater than 0
             // If it's less than 0 then overflow into the count of the mp3 folder.
+            previousIndex = currentIndex;
 
-            throw new NotImplementedException();
+            if(currentIndex == indexMin)
+                currentIndex = indexMax;
+            else
+                currentIndex -= 1;
         }
 
-        public int IndexNext(int currentIndex, string path, int mp3Count)
+        /// <summary>
+        /// Moves index forward by 1
+        /// </summary>
+        public void IndexNext()
         {
             // Increment currentIndex and check if it's under the max ammount
             // If it's greater than the max amount then overflow into 0 otherwise increase it.
+            previousIndex = currentIndex;
 
-            throw new NotImplementedException();
+            if (currentIndex == indexMax)
+                currentIndex = indexMin;
+            else
+                currentIndex += 1;
         }
 
-        public int ShuffleIndex()
+        /// <summary>
+        /// Sets current index to a random number.
+        /// </summary>
+        public void RandomIndex()
         {
             // Shuffle the index by generating a random index number between 0 and the max ammount
 
-            throw new NotImplementedException();
+
         }
     }
 }
