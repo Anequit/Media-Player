a4db7e8e695acdd01597e050b6b004d6e2e4257c
Bailey Roach <Anequit@gmail.com>
2021-03-26 13:11:21 -0400

    Clean up

--- MediaPlayerUI/Form1.cs
+++ MediaPlayerUI/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MediaControllerLibrary;
+using MediaControllerLibrary.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -27,130 +29,46 @@ namespace MediaPlayerUI
         bool shuffling = false;
 
         **/
+        readonly FileHandler fileHandler;
+        readonly MediaHandler mediaHandler;
+
         public MediaPlayerForm()
         {
             InitializeComponent();
 
-            // On start up initialize FileHandler and fetch mp3 files
-            //  
-            //
+            fileHandler = new FileHandler(FileType.mp3);
+            fileHandler.BuildFileList();
 
+            mediaHandler = new MediaHandler(fileHandler.GetFileList());
 
-            /**
-            if (!Directory.Exists(@".\Media"))
-            {
-                Directory.CreateDirectory(@".\Media");
-            }
-            
-            List<FileInfo> files = controller.GetMediaMp3(Path);
-
-            if(files.Count == 0)
-            {
-                MessageBox.Show("Media contains no mp3 files", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Process.Start(@".\Media");
-                Environment.Exit(1);
-            }
-
-            player.Open(controller.MediaPath(Path, files[index]));
-
-            player.MediaEnded += Player_MediaEnded;
-
-            var Mp3 = controller.GetMediaMp3(Path);
-
-            Songname_LBL.Text = Mp3[index].Name;
+            Songname_LBL.Text = mediaHandler.GetCurrentSong();
             volume_lbl.Text = (Volume.Value / 1).ToString();
-            **/
         }
 
-        private void Player_MediaEnded(object sender, EventArgs e)
-        {
-            /**
-            var Mp3 = controller.GetMediaMp3(Path);
+        private void Start_BTN_Click(object sender, EventArgs e) => mediaHandler.Play();
 
-            if (!repeating)
-            {
-                if (!shuffling)
-                    index = indexHelper.IndexQueueNext(index, Path, controller);
-                else
-                    index = indexHelper.ShuffleIndex(index, pastIndex, random, Mp3);
-            }
-            
-            player.Open(controller.MediaPath(Path, Mp3[index]));
-            player.Play();
-            Songname_LBL.Text = Mp3[index].Name;
-            **/
-        }
-
-        private void Start_BTN_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Stop_BTN_Click(object sender, EventArgs e)
-        {
-            //player.Pause();
-        }
+        private void Stop_BTN_Click(object sender, EventArgs e) => mediaHandler.Stop();
 
         private void Next_BTN_Click(object sender, EventArgs e)
         {
-            /**
-            var Mp3 = controller.GetMediaMp3(Path);
-
-            if (!shuffling)
-                index = indexHelper.IndexQueueNext(index, Path, controller);
-            else
-                index = indexHelper.ShuffleIndex(index, pastIndex, random, Mp3);
-            
-            player.Open(controller.MediaPath(Path, controller.GetMediaMp3(Path)[index]));
-            player.Play();
-            Songname_LBL.Text = Mp3[index].Name;
-            **/
+            Songname_LBL.Text = mediaHandler.GetCurrentSong();
+            mediaHandler.Next();
         }
 
         private void Back_BTN_Click(object sender, EventArgs e)
         {
-            /**
-            List<FileInfo> Mp3 = controller.GetMediaMp3(Path);
-
-            if (!shuffling)
-                index = indexHelper.IndexQueueBack(index, Path, controller);
-            else
-                index = indexHelper.ShuffleIndex(index, pastIndex, random, Mp3);
-
-            player.Open(controller.MediaPath(Path, controller.GetMediaMp3(Path)[index]));
-            player.Play();
-            Songname_LBL.Text = Mp3[index].Name;
-            **/
+            Songname_LBL.Text = mediaHandler.GetCurrentSong();
+            mediaHandler.Back();
         }
 
         private void Volume_Scroll(object sender, EventArgs e)
         {
-            /**
-            volume_lbl.Text = (Volume.Value  / 1).ToString();
-            player.Volume = (Volume.Value * 0.01);
-            **/
+            volume_lbl.Text = (Volume.Value / 1).ToString();
+            mediaHandler.ChangeVolume(Volume.Value);
         }
 
-        private void Repeat_BTN_CheckedChanged(object sender, EventArgs e)
-        {
-            /**
-            if (Repeat_BTN.CheckState == CheckState.Checked)
-                repeating = true;
+        private void Repeat_BTN_CheckedChanged(object sender, EventArgs e) => mediaHandler.ToggleRepeat();
 
-            if (Repeat_BTN.CheckState == CheckState.Unchecked)
-                repeating = false;
-            **/
-        }
-
-        private void Shuffle_BTN_CheckedChanged(object sender, EventArgs e)
-        {
-            /**
-            if (Shuffle_BTN.CheckState == CheckState.Checked)
-                shuffling = true;
-
-            if (Shuffle_BTN.CheckState == CheckState.Unchecked)
-                shuffling = false;
-            **/
-        }
+        private void Shuffle_BTN_CheckedChanged(object sender, EventArgs e) => mediaHandler.ToggleShuffle();
     }
 }
