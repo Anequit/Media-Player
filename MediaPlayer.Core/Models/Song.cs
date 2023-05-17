4ab1b373bcf2849a7ebbb65e6ffdc839fc552948
Bailey <Anequit@gmail.com>
2023-05-17 04:11:56 -0400

    Add Song

---
+++ MediaPlayer.Core/Models/Song.cs
@@ -0,0 +1,13 @@
+﻿namespace MediaPlayer.Core.Models;
+
+public class Song
+{
+    public Song(string path, string name)
+    {
+        Path = path;
+        Name = name;
+    }
+
+    public string Path { get; set; }
+    public string Name { get; set; }
+}
